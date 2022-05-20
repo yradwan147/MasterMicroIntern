e7350eb72bc75a32c180436f87aa4417e676c6cd
Yousef Radwan <54478004+yradwan147@users.noreply.github.com>
2022-05-20 10:22:56 +0200

    Task 3 Complete

--- task3/task3.py
+++ task3/task3.py
@@ -1,28 +1,35 @@
-import numpy as np
+def flatten_matrix():
 
-rows = input("Enter number of rows: ")
-matrix = []
-print('Enter your row element lists one by one separated by commas i.e "[2,3,5],[3,4],[4,7,8],[5,8,9,10]", can be any number of channels and any number of elements per row')
-for i in range(eval(rows)):
-    input_list = eval('['+input('')+']')
-    matrix.append(input_list)
+    rows = input("Enter number of rows: ")
+    matrix = []
+    print('Enter your row element lists one by one separated by commas i.e "[2,3,5],[3,4],[4,7,8],[5,8,9,10]", can be any number of channels and any number of elements per row')
+    # Convert string input into list
+    for i in range(eval(rows)):
+        input_list = eval('['+input('')+']')
+        matrix.append(input_list)
 
-print(matrix)
+    print(matrix)
 
-out = []
+    out = []
 
-def ravel(in_list):
-    for elem in in_list:
-        if isinstance(elem, list):
-            ravel(elem)
-        else:
-            out.append(elem)
+    # Recursive function to pull out all matrix elements and put them into out
+    def ravel(in_list):
+        for elem in in_list:
+            # if the element is a list, recall the function on it
+            if isinstance(elem, list):
+                ravel(elem)
+            # else, add it to out
+            else:
+                out.append(elem)
+    
+    # Run the function on inputted matrix
+    ravel(matrix)
 
-ravel(matrix)
+    return out
 
-print(out)
+print(flatten_matrix())
+
 
 # Wasn't used because it doesn't support different number of elements or channels in each row. 
-# Plus, the above implementation is already a similar implementation to the code under the wrapper function ravel()
+# Plus, the above implementation is already a similar implementation to the code under the wrapper function ravel() in numpy
 # res = np.array(matrix, dtype=object).ravel()
-# print(res[0], res[5], res[8])
